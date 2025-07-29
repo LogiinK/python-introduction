7796a5442bbbd7fad903d2fa0e037ca3b244ba2f
login <loginthefalle@mail.com>
2025-07-29 10:55:39 +0200

    addition a + b

--- exercises/4-fizzbuzz/src/fizzbuzz.py
+++ exercises/4-fizzbuzz/src/fizzbuzz.py
@@ -1,2 +1,18 @@
+from numpy import number
 def fizzbuzz(number: int) -> str:
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    if number % 3 == 0 :
+        return "Fizz"
+    if number % 5 == 0 :
+        return "Buzz"
+
+
+
     return str(number)
+
+def fizzbuzzfrom_range(lower: int, upper: int) -> str:
+    result = ""
+    for number in range(lower, upper+1):
+        result +=fizzbuzz(number)
+    return result
--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,19 @@
-from src import fizzbuzz
+from src import *
 
-# def test_fizzbuzz():
-#     assert fizzbuzz(1)
+def test_1_should_return_1():
+    assert fizzbuzz(1) == "1"
+
+def test_2_should_return_2():
+    assert fizzbuzz(2) == "2"
+
+def test_3_should_return_3():
+    assert fizzbuzz(3) == "Fizz"
+
+def test_5_should_return_Buzz():
+    assert fizzbuzz(5) == "Buzz"
+
+def test_15_should_return_FizzBuzz():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+def test_fizzbuzz_from_range():
+    assert fizzbuzzfrom_range(1, 15) == "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz"
