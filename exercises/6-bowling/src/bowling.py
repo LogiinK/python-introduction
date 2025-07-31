26da77830907c115e93f7d2adc41151e6e7e0bac
login <loginthefalle@mail.com>
2025-07-31 15:11:48 +0200

    extract function

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -1,12 +1,17 @@
 
+def get_or_default(frames: list[str], index: int) -> str:
+    try:
+        return frames[index]
+    except IndexError:
+        return "-"
 
 def bowling2strike(partie: str) -> int:
     frames = partie.split()
     score = 0
 
-    for i, frame in enumerate(frames):
+    for i, frame in enumerate(frames[:10]):
         if frame == "X":
-            score += handle_strike()
+            score += handle_strike(get_or_default(frames, i +1), get_or_default(frames, i + 2))
         else:
             for throw in frame:
                 if throw == "-":
@@ -16,5 +21,33 @@ def bowling2strike(partie: str) -> int:
 
     return score
 
-def handle_strike() -> int:
-    return 10
+def handle_strike(next_frame: str, next_next_frame: str) -> int:
+    next_throw: int = 0
+    next_next_throw: int = 0
+
+    if next_frame == "X":
+        next_throw = 10
+
+        if next_next_frame == "X":
+            next_next_throw = 10
+        elif next_next_frame in ["-", "--"]:
+            next_next_throw = 0
+        else:
+            if next_next_frame[0] == "-":
+                next_next_throw += 0
+            else:
+                next_next_throw += int(next_next_frame[0])
+
+    elif next_frame in ["-", "--"]:
+        next_throw = 0
+
+    else :
+        next_frame2 = list(next_frame)
+        for throw in next_frame2:
+            if throw == "-":
+                next_throw += 0
+            else:
+                next_throw += int(throw)
+
+
+    return 10+ next_throw + next_next_throw
--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -11,3 +11,9 @@ def test_bowling2strike():
 
 def test_strike_with_throws_after():
     assert bowling2strike("X 25 22 12 -- 81 11 -- -- 22") == 46
+
+def test_strike_perfect_with_throws_after():
+    assert bowling2strike("X X X X X X X X X X X X") == 300
+
+def test_spare_with_throws_after():
+    assert bowling2strike("X 25 2/ -- 9/ 11 -- -- 26") == 58
